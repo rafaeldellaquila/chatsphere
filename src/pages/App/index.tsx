09a8005ddaee83d0e7457b27a192ef4889498316
rafaeldellaquila <rafael@dellaquila.dev>
2024-05-20 18:16:21 -0300

    :sparkles: create the page

---
+++ src/pages/App/index.tsx
@@ -0,0 +1,12 @@
+import CardChat from '@components/CardChat'
+import SContainer from '@components/Container/styles'
+
+import '@i18n/index'
+
+export default function App() {
+  return (
+    <SContainer>
+      <CardChat />
+    </SContainer>
+  )
+}
