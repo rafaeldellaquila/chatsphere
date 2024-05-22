416ad4b39f37fc8b24eda8ae4716de21b4266ba3
rafaeldellaquila <rafael@dellaquila.dev>
2024-05-22 20:42:47 -0300

    :lipstick: fix app

--- src/pages/App/index.tsx
+++ src/pages/App/index.tsx
@@ -1,12 +1,7 @@
-import CardChat from '@components/CardChat'
 import SContainer from '@components/Container/styles'
 
 import '@i18n/index'
 
 export default function App() {
-  return (
-    <SContainer>
-      <CardChat />
-    </SContainer>
-  )
+  return <SContainer></SContainer>
 }
